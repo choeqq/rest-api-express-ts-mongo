1829f166d0f1a4d252b2f2f5a86c5ad7f1e86053
choeqq <k.chola@op.pl>
2022-08-22 19:44:46 +0200

    boostrap app, db connection established

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.listen(port, async () => {
+  console.log("this app is running");
+
+  await connect();
+});
